344973b716364ec7ff0ff4cae1f784cdbbe06408
Aanandan-T-Ma <aanandan.tma@gmail.com>
2021-04-21 10:47:20 +0530

    Sign up Validation

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react'
 import { Form,FormGroup,Label,Input,Nav,NavItem,TabContent,TabPane } from 'reactstrap'
 import '../styles/login.css'
 
+const emailRegex = [
+    {
+        regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        error: 'Invalid Email!'
+    }
+]
+const usernameRegex = [
+    {
+        regex: /^[a-zA-Z].{7,}$/,
+        error: 'Username should start with a letter and contain atleast 8 characters'
+    },
+    {
+        regex: /^[a-zA-Z0-9_]*$/,
+        error: 'Username should contain only letters, numbers and underscore(_)'
+    }
+]
+const passwordRegex = [
+    {
+        regex: /^[a-zA-Z0-9_@#$&]*$/,
+        error: 'Password should contain only letters,numbers,_,@,#,$,&'
+    },
+    {
+        regex: /^.{8,}$/,
+        error: 'Password should contain atleast 8 characters'
+    }
+]
+
 class Login extends Component {
     constructor(props)
     {
@@ -12,7 +39,13 @@ class Login extends Component {
             username:'',
             password:'',
             confirmpassword:'',
-            logintype:'user'
+            logintype:'user',
+            errors: {
+                email:'',
+                username:'',
+                password:'',
+                confirmpassword:''
+            }
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
@@ -22,9 +55,7 @@ class Login extends Component {
     }
 
     handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
+        const { name, value } = event.target;
         
         this.setState({
           [name]: value
@@ -57,34 +88,51 @@ class Login extends Component {
     }
     handleSignupSubmit(event)
     {
-        let pattern = new RegExp("[a-zA-Z]{1}[a-zA-Z0-9_]{2,}")
-        
-        if(pattern.test(this.state.username))
-        {
-            pattern=new RegExp("[a-zA-Z]{1}[a-zA-Z0-9_@#$&]{2,}")
-            if(pattern.test(this.state.password))
-            {
-                if(this.state.password!==this.state.confirmpassword)
-                {
-                    alert('Password and confirm password does not match')
-                }
-                else
-                {
-                    alert('Current State is: ' + JSON.stringify(this.state))
-                }
-            }
-            else
-            {
-                alert('Password should start with a-z or A-Z and can contain @,#,$,&,_')
-            }
-        }
-        else
-        {
-            window.alert('Username should start with a-z or A-Z and can contain a-z,A-Z,0-9,_ and should be minimum of 3 characters')
-        }
-        
-        
         event.preventDefault();
+        let errors = {
+            email: '',
+            username: '',
+            password: '',
+            confirmpassword: ''
+        }
+        this.setState({
+            errors: errors
+        })
+        for(let i in emailRegex){
+            if(!emailRegex[i].regex.test(this.state.email)){
+                errors.email = emailRegex[i].error
+                break
+            }
+        }
+        for(let i in usernameRegex){
+            if(!usernameRegex[i].regex.test(this.state.username)){
+                errors.username = usernameRegex[i].error
+                break
+            }
+        }
+        for(let i in passwordRegex){
+            if(!passwordRegex[i].regex.test(this.state.password)){
+                errors.password = passwordRegex[i].error
+                break
+            }
+        }
+        if(this.state.confirmpassword !== this.state.password)
+            errors.confirmpassword = 'Passwords don\'t match'
+        let isError = false
+        for(let field in errors){
+            if(errors[field] !== ''){
+                isError = true
+                break
+            }
+        }
+        if(isError){
+            this.setState({
+                errors: errors
+            })
+            return
+        }
+
+        alert('Current State is: ' + JSON.stringify(this.state))
     }
 
     render(){
@@ -106,10 +154,12 @@ class Login extends Component {
                         <div className="col-4 login-form-bg">
                             <div className="row">
                                 <Nav className="login-nav">
-                                        <NavItem className={(this.state.formtype==='login')?'login-activate-tab col-6':'login-navitem col-6'} onClick={() => this.changeForm('login')} >
+                                        <NavItem className={(this.state.formtype==='login')?'login-activate-tab col-6':'login-navitem col-6'} 
+                                                onClick={() => this.changeForm('login')} >
                                             Login
                                         </NavItem>
-                                        <NavItem className={(this.state.formtype==='signup')?'login-activate-tab col-6':'login-navitem col-6'} onClick={() => this.changeForm('signup')} >
+                                        <NavItem className={(this.state.formtype==='signup')?'login-activate-tab col-6':'login-navitem col-6'} 
+                                                onClick={() => this.changeForm('signup')} >
                                             Sign Up
                                         </NavItem>
                                 </Nav>
@@ -123,19 +173,24 @@ class Login extends Component {
                                         <h1 className="login-form-header"><b>Login</b></h1>
                                         <FormGroup row>
                                                 <Label htmlFor="username" className="col-12">Username</Label>
-                                                <Input type="text" className="login-input-box col-12" id="username" name="username" placeholder="Username" autoComplete="off" required value={this.state.username} onChange={this.handleInputChange} />
+                                                <Input type="text" className="login-input-box col-12" id="username" name="username" 
+                                                        placeholder="Username" autoComplete="off" required value={this.state.username} 
+                                                        onChange={this.handleInputChange} />
                                         </FormGroup>
                                         <FormGroup row>
                                                 <Label htmlFor="password" className="col-12">Password</Label>
-                                                <Input type="password" className="login-input-box col-12" id="password" name="password" placeholder="Password" autoComplete="off" required value={this.state.password} onChange={this.handleInputChange} />
+                                                <Input type="password" className="login-input-box col-12" id="password" name="password" 
+                                                        placeholder="Password" autoComplete="off" required value={this.state.password} 
+                                                        onChange={this.handleInputChange} />
                                         </FormGroup>
                                         <FormGroup row>
-                                            <Input type="checkbox" className="ml-1" onClick={this.showPassword} />
+                                            <Input type="checkbox" id="showpassword" className="ml-1" onClick={this.showPassword} />
                                             <Label htmlFor="showpassword" className="ml-4"> Show Password</Label>
                                         </FormGroup>
                                         <FormGroup row className="mt-4">
                                                 <Label htmlFor="logintype" className="col-4">Login as</Label>
-                                                <Input className="col-8" type="select" id="logintype" name="logintype" required value={this.state.logintype} onChange={this.handleInputChange} >
+                                                <Input className="col-8" type="select" id="logintype" name="logintype" required 
+                                                        value={this.state.logintype} onChange={this.handleInputChange} >
                                                     <i className="fas fa-chevron-down"></i>
                                                     <option>user</option>
                                                     <option>doctor</option>
@@ -143,7 +198,9 @@ class Login extends Component {
                                                 </Input>
                                         </FormGroup>
                                         <FormGroup row className="justify-content-center mt-4">
-                                            <Input type="submit" value="Login" className="login-submit-btn mb-3" />
+                                            <div className="col-5">
+                                                <Input type="submit" value="Login" className="btn login-submit-btn mb-3" />
+                                            </div>
                                         </FormGroup>
                                     </Form>
                                 </TabPane>
@@ -155,30 +212,45 @@ class Login extends Component {
                                         <h1 className="login-form-header"><b>Sign Up</b></h1>
                                         <FormGroup row>
                                                 <Label htmlFor="email" className="col-12">Email Address</Label>
-                                                <Input type="email" className="login-input-box col-12" id="email" name="email" placeholder="Email" autoComplete="off" required value={this.state.email} onChange={this.handleInputChange} />
+                                                <Input type="text" className="login-input-box col-12" id="email" name="email" 
+                                                        placeholder="Email" autoComplete="off" required value={this.state.email} 
+                                                        onChange={this.handleInputChange} />
+                                                <span className="col-12 error">{ this.state.errors.email }</span>
                                         </FormGroup>
                                         <FormGroup row>
                                                 <Label htmlFor="username" className="col-12">Username</Label>
-                                                <Input type="text" className="login-input-box" id="username" name="username" placeholder="Username" autoComplete="off" required value={this.state.username} onChange={this.handleInputChange} />
+                                                <Input type="text" className="login-input-box" id="username" name="username" 
+                                                        placeholder="Username" autoComplete="off" required value={this.state.username} 
+                                                        onChange={this.handleInputChange} />
+                                                <span className="col-12 error">{ this.state.errors.username }</span>
                                         </FormGroup>
                                         <FormGroup row>
                                                 <Label htmlFor="password" className="col-12">Password</Label>
-                                                <Input type="password" className="login-input-box" id="password" name="password" placeholder="Password" autoComplete="off" required value={this.state.password} onChange={this.handleInputChange} />
+                                                <Input type="password" className="login-input-box" id="password" name="password" 
+                                                        placeholder="Password" autoComplete="off" required value={this.state.password} 
+                                                        onChange={this.handleInputChange} />
+                                                <span className="col-12 error">{ this.state.errors.password }</span>
                                         </FormGroup>
                                         <FormGroup row>
                                                 <Label htmlFor="confirmpassword" className="col-12">Confirm Password</Label>
-                                                <Input type="password" className="login-input-box" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password" autoComplete="off" required value={this.state.confirmpassword} onChange={this.handleInputChange} />
+                                                <Input type="password" className="login-input-box" id="confirmpassword" name="confirmpassword" 
+                                                        placeholder="Confirm Password" autoComplete="off" required value={this.state.confirmpassword} 
+                                                        onChange={this.handleInputChange} />
+                                                <span className="col-12 error">{ this.state.errors.confirmpassword }</span>
                                         </FormGroup>
                                         <FormGroup row className="mt-4">
                                                 <Label htmlFor="logintype" className="col-4">Type</Label>
-                                                <Input className="col-8 ml-auto" type="select" id="logintype" name="logintype" required value={this.state.logintype} onChange={this.handleInputChange} >
+                                                <Input className="col-8 ml-auto" type="select" id="logintype" name="logintype" required 
+                                                        value={this.state.logintype} onChange={this.handleInputChange} >
                                                     <i className="fas fa-chevron-down"></i>
                                                     <option>user</option>
                                                     <option>doctor</option>
                                                 </Input>
                                         </FormGroup>
                                         <FormGroup row className="justify-content-center mt-4">
-                                            <Input type="submit" value="Sign up" className="login-submit-btn mb-3" />
+                                            <div className="col-5">
+                                                <Input type="submit" value="Sign up" className="btn login-submit-btn mb-3" />
+                                            </div>
                                         </FormGroup>
                                     </Form>
                                 </TabPane>
