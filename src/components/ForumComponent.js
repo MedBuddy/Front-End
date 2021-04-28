fe30c6ae6fa5fbb605de6bee67af623132cabb03
sprasanth2k <sprasanth2k@gmail.com>
2021-04-28 21:31:48 +0530

    forum inital change

--- src/components/ForumComponent.js
+++ src/components/ForumComponent.js
@@ -1,67 +1,114 @@
-import React, { Component } from 'react'
-import Header from './HeaderComponent'
-import  '../styles/forum.css'
-import { Form, Input,Card,CardBody,CardTitle,CardText} from 'reactstrap';
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import  '../styles/forum.css';
+import { Input,Card,CardBody,CardText } from 'reactstrap';
 
 class Forum extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+
+        }
+        this.searchTopic = this.searchTopic.bind(this);
+    }
+
+    searchTopic(event)
+    {
+        if(event.charCode===13)
+        {
+            alert('enter pressed')
+        }
+    }
+
     render(){
         return (
             <>
                 <Header />
-                <div class="container">
-                <div className="row">
-                                    <div className="col-1 mt-3">
-                                        <img src="/images/fire.jpg" class="forum-fire-image" alt="MedBuddy" width="55" height="50" />
-                                    </div>
-                                    <div className="col-4 mt-2 forum-trend">Trending Today</div>
-                                    <div class="col-4  mt-4">
-                                        <Form>
-                                            <Input type="search" className="forum-search" name="searchBar" id="searchBarId" placeholder="Search topics" />
-                                        </Form>
-                                    </div>
-                                    <div class="col-2  mt-4">
-                                        <Form>
-                                            <Input type="submit" className="forum-submit" name="searchButton" id="searchButtonId" value="Search" />
-                                        </Form>
-                                    </div>
-                </div>
-                <div className="row mt-5 ml-5">
-                    <div className="col-3">
-                        <Card className="forum-card">
-                            <CardBody>
-                                <CardTitle className="forum-card-title">Title 1</CardTitle>
-                                <CardText className="forum-card-text">This is a Card text</CardText>
-                            </CardBody>
-                        </Card>
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="col-md-4 forum-trending-today">
+                            Trending Today
+                        </div>
+                        <div className="col-md-5 offset-md-2">
+                            
+                            <Input type="text" className="forum-search" placeholder="Search Topics" onKeyPress={this.searchTopic} />
+                            {/* <i className="fa fa-search"></i> */}
+                        </div>
                     </div>
-                    <div className="col-3 offset-1">
-                        <Card className="forum-card">
+                    <div className="row mt-5">
+                        <div className="col-md-3">
+                            <Card>
                                 <CardBody>
-                                    <CardTitle className="forum-card-title">Title 2</CardTitle>
-                                    <CardText className="forum-card-text">This is a Card text</CardText>
+                                    <CardText>
+                                        Trending question-1
+                                    </CardText>
+                                    <CardText>
+                                        View entire discussion
+                                    </CardText>
                                 </CardBody>
-                        </Card>  
-                    </div>
-                    <div className="col-3 offset-1">
-                        <Card className="forum-card">
+                            </Card>
+                        </div>
+                        <div className="col-md-3 offset-md-1">
+                            <Card>
                                 <CardBody>
-                                    <CardTitle className="forum-card-title">Title 3</CardTitle>
-                                    <CardText className="forum-card-text">This is a Card text</CardText>
+                                    <CardText>
+                                        Trending question-2
+                                    </CardText>
+                                    <CardText>
+                                        View entire discussion
+                                    </CardText>
                                 </CardBody>
-                        </Card>  
+                            </Card>
+                        </div>
+                        <div className="col-md-3 offset-md-1">
+                            <Card>
+                                <CardBody>
+                                    <CardText>
+                                        Trending question-3
+                                    </CardText>
+                                    <CardText>
+                                        View entire discussion
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
                     </div>
-                </div>
-                <div className="row mt-5 ">
-                    <div className="col-2 bg-light" id="button-back">
-                        <Input type="submit" className="forum-submit" value="Most Popular" name="most-popular" id="most-popular-id" />
+                    <div className="row mt-5 align-items-center">
+                        
+                            <div className="col-md-8">
+                                <div className="forum-discussion-sort">
+                                    <div className="row justify-content-center">
+                                        <div className="col-3">
+                                            <div className="forum-discussion-sort-heading p-1">
+                                                Most Popular
+                                            </div>
+                                        </div>
+                                        <div className="col-3 offset-1">
+                                            <div className="forum-discussion-sort-heading p-1">
+                                                Highest Votes
+                                            </div>
+                                        </div>
+                                        <div className="col-3 offset-1">
+                                            <div className="forum-discussion-sort-heading p-1">
+                                                Latest Talks
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        <div className="col-md-3 offset-md-1 ">
+                            <div className="forum-ask-question-bg">
+                                <a href="" className="forum-ask-question">Ask Your question</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-2 bg-light" id="button-back">
-                        <Input type="submit" className="forum-submit" value="Highest Votes" name="highest-votes" id="highest-votes-id" />
+                    <div className="row mt-4">
+                        <div className="col-12 forum-discussion">
+                            Discussions
+                        </div>
                     </div>
-                    <div className="col-2 bg-light" id="button-back">
-                        <Input type="submit" className="forum-submit" value="Latest Talks" name="latest-talks" id="latest-talks-id"/>
-                    </div>
-                </div>
                 </div>
             </>
         )
