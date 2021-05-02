f3f9f1dad31930cd2bb7125d5546f440c0dd16ea
sprasanth2k <sprasanth2k@gmail.com>
2021-05-02 18:38:42 +0530

    forum discussions completed

--- src/components/ForumComponent.js
+++ src/components/ForumComponent.js
@@ -9,15 +9,24 @@ class Forum extends Component {
         super(props);
         this.state = {
             questions: [],
+            files:[],
             modal: false,
+            discussionType: 1,
         }
         this.searchTopic = this.searchTopic.bind(this);
         this.fetchDiscussions = this.fetchDiscussions.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.postQuestion = this.postQuestion.bind(this);
         this.checkLogin = this.checkLogin.bind(this);
+        this.handleFileInput = this.handleFileInput.bind(this);
+        this.changeDiscussionType = this.changeDiscussionType.bind(this);
     }
-
+    handleFileInput(event)
+    {
+        this.setState({
+            files: event.target.files
+        })
+    }
     componentDidMount(){
         this.fetchDiscussions()
     }
@@ -58,26 +67,41 @@ class Forum extends Component {
             .then(response => response.json())
             .then((response) => {
                 this.setState({
-                    questions: response
+                    questions: response.reverse()
                 })
             })
     }
     renderDiscussions()
     {
-        const questions=this.state.questions;
-        console.log(this.state.questions);
-        const x = questions.map((question) => 
-            <Media>
-                <Media body className="ml-5">
-                    <Media heading>{question.title}</Media>
-                    <p>{question.content}</p>
-                    <a href="">View entire discussion</a>
+        
+        const questions = this.state.questions.map((question) => {
+            let d = new Date(Date.parse(question.createdAt));
+            let hh = d.getHours();
+            let mm = d.getMinutes();
+            let time = hh + ":" + mm;
+            return(
+                <Media className="forum-question-render mb-3">
+                    <Media left middle className="ml-2 forum-discussion-image-container">
+                        <Media object src={question.userIcon.url} alt={question.askedUserName} className="forum-discussion-image" />
+                        <Media body>{question.askedUserName}</Media>
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading className="pt-2">{question.title}</Media>
+                        <p>{question.content}</p>
+                    </Media>
+                    <Media right className="mt-auto mr-3 forum-discussion-date">
+                        <Media>
+                            ~ {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(d)+' ⏰'+time}
+                        </Media>
+                        
+                    </Media>
                 </Media>
-            </Media>
+            )
+            }
         )
         return(
-            <Media list>
-                {x}
+            <Media list className="col-12">
+                {questions}
             </Media>
         )
     }
@@ -93,11 +117,13 @@ class Forum extends Component {
     }
     postQuestion(event)
     {
-        this.toggleModal();
+        
         const userToken = localStorage.getItem('userToken');
         let question = new FormData()
         question.append('title', this.topic.value)
         question.append('content', this.question.value)
+        for(let i=0;i<this.state.files.length;i++)
+            question.append('image',this.state.files[i])
         fetch('queries/',{
             method: 'POST',
             headers: {
@@ -107,13 +133,86 @@ class Forum extends Component {
         })
         .then((response) => {
             if(response.ok)
-                alert('posted succesfully');
+            {
+                console.log('posted succesfully');
+                this.toggleModal();
+                window.location.reload();
+            }
+            else if(response.status === 500 )
+            {
+                alert('you can upload only 3 files...!!')
+            }
+            else
+            {
+                let error = new Error('Error: ' + response.status + ': ' + response.statusText)
+                error.response = response
+                console.log(typeof(response.status))
+                throw error
+            }
         }, err => {
             let error = new Error(err)
             throw error
         })
-        
+        event.preventDefault()
     }
+    findUpvote(replies)
+    {
+        let i,sum=0;
+        for(i=0;i<replies.length;i++)
+            sum+=replies.upvotes.length
+        return sum;
+    }
+    changeDiscussionType(event)
+    {
+        let id = parseInt(event.target.id);
+        if(id !== this.state.discussionType)
+        {
+            
+            let questions = this.state.questions
+            if(id === 1)
+            {
+                questions.sort(
+                    function(a,b){
+                        if(a.createdAt > b.createdAt)
+                            return -1;
+                        else if(a.createdAt < b.createdAt)
+                            return 1;
+                        return 0;
+                    }
+                )
+            }   
+            else if(id === 2)
+            {
+                questions.sort(
+                    function(a,b){
+                        let a_up = (a.replies.length)?this.findUpvote(a.replies):0,b_up = (a.replies.length)?this.findUpvote(b.replies):0;
+                        if(a_up > b_up)
+                            return -1;
+                        else if(a_up < b_up)
+                            return 1;
+                        return 0;
+                    }
+                )
+            }
+            else
+            {
+                questions.sort(
+                    function(a,b){
+                        if(a.replies.length > b.replies.length)
+                            return -1;
+                        else if(a.replies.length < b.replies.length)
+                            return 1;
+                        return 0;
+                    }
+                )
+            }
+            this.setState({
+                discussionType: id,
+                questions: questions
+            })
+        }
+    }
+
     render(){
         return (
             <>
@@ -173,18 +272,18 @@ class Forum extends Component {
                                 <div className="forum-discussion-sort">
                                     <div className="row justify-content-center">
                                         <div className="col-3">
-                                            <div className="forum-discussion-sort-heading p-1">
-                                                Most Popular
+                                            <div className={(this.state.discussionType === 1)?"forum-discussion-sort-heading-active p-1":"forum-discussion-sort-heading p-1"} id="1" onClick={this.changeDiscussionType}>
+                                                Latest Talks
                                             </div>
                                         </div>
                                         <div className="col-3 offset-1">
-                                            <div className="forum-discussion-sort-heading p-1">
+                                            <div className={(this.state.discussionType === 2)?"forum-discussion-sort-heading-active p-1":"forum-discussion-sort-heading p-1"} id="2" onClick={this.changeDiscussionType}>
                                                 Highest Votes
                                             </div>
                                         </div>
                                         <div className="col-3 offset-1">
-                                            <div className="forum-discussion-sort-heading p-1">
-                                                Latest Talks
+                                            <div className={(this.state.discussionType === 3)?"forum-discussion-sort-heading-active p-1":"forum-discussion-sort-heading p-1"} id="3" onClick={this.changeDiscussionType}>
+                                                Most Popular
                                             </div>
                                         </div>
                                     </div>
@@ -214,8 +313,8 @@ class Forum extends Component {
                                                 innerRef={(input) => this.question = input} />
                                         </FormGroup>
                                         <FormGroup>
-                                            <Label htmlFor="files">Images (max. 3)</Label>
-                                            <Input type="file" id="files" name="files" 
+                                            <Label htmlFor="image">Images (max. 3)</Label>
+                                            <Input type="file" id="image" name="image" multiple onChange={this.handleFileInput} accept="image/*"
                                                 />
                                         </FormGroup>
                                     </Form>
@@ -228,13 +327,10 @@ class Forum extends Component {
                         </div>
                     </div>
                     <div className="row mt-4">
-                        <div className="col-12 forum-discussion">
+                        <div className="col-12 forum-discussion mb-4">
                             Discussions
                         </div>
-                        <div className="col-12">
-                                {this.renderDiscussions()}
-
-                        </div>
+                        {this.renderDiscussions()}
                     </div>
                 </div>
             </>
