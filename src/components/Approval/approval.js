7684c10e933c0de46b9ee667d2baa09de9fe8629
sprasanth2k <sprasanth2k@gmail.com>
2021-05-18 17:14:17 +0530

    approval page done

--- src/components/Approval/approval.js
+++ src/components/Approval/approval.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Header from '../Header/header';
 import { ScaleLoader } from 'react-spinners';
+import { Modal,ModalHeader,ModalBody,ModalFooter,Form,Label,Input,FormGroup,Button } from 'reactstrap';
 import './approval.css';
 
 class Approval extends Component {
@@ -10,13 +11,38 @@ class Approval extends Component {
         this.state = {
             loading: true,
             doctors: [],
+            approveModal: false,
+            rejectModal: false,
+            doctorIndex: -1,
         }
         this.fetchApproval = this.fetchDoctors.bind(this);
+        this.toggleapproveModal = this.toggleapproveModal.bind(this);
+        this.togglerejectModal = this.togglerejectModal.bind(this);
+        this.approveDoctor = this.approveDoctor.bind(this);
+        this.rejectDoctor =this.rejectDoctor.bind(this);
+        this.renderApproveModal = this.renderApproveModal.bind(this);
+        this.renderRejectModal = this.renderRejectModal.bind(this);
     }
 
     componentDidMount()
     {
         this.fetchDoctors();
+    }
+
+    toggleapproveModal(index=-1)
+    {
+        this.setState({
+            approveModal: !this.state.approveModal,
+            doctorIndex: index
+        })
+    }
+
+    togglerejectModal(index=-1)
+    {
+        this.setState({
+            rejectModal: !this.state.rejectModal,
+            doctorIndex: index
+        })
     }
     
     fetchDoctors()
@@ -71,7 +97,7 @@ class Approval extends Component {
         }
         else
         {
-            const doctors = this.state.doctors.map((doctor) => {
+            const doctors = this.state.doctors.map((doctor,index) => {
                 return(
                     <div className="approval-doctor pt-3 pb-4">
                         <div className="approval-doctor-name">
@@ -84,8 +110,8 @@ class Approval extends Component {
                             <a href={doctor.license} target="_blank" rel="noreferrer" >View license</a>
                         </div>
                         <div className="pt-3">
-                            <span className="btn btn-success">Approve</span>
-                            <span className="btn btn-danger ml-2">Reject</span>
+                            <span className="btn btn-success" onClick={() => this.toggleapproveModal(index)}>Approve</span>
+                            <span className="btn btn-danger ml-2" onClick={() => this.togglerejectModal(index)}>Reject</span>
                         </div>
                     </div>
                 )
@@ -105,6 +131,129 @@ class Approval extends Component {
                 </>
             )
         }
+    }
+
+    approveDoctor(event)
+    {
+        event.preventDefault();
+        let doctors = this.state.doctors;
+        const userToken = localStorage.getItem('userToken');
+        const content = {
+            doctorId: doctors[this.state.doctorIndex]._id,
+            specialization: this.specialization.value,
+        }
+        fetch('/admin/acceptDoctor',{
+            method: 'PUT',
+            headers:{
+                'Authorization': 'Bearer ' + userToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(content)
+        })
+        .then((response) => {
+            if(response.ok)
+            {
+                this.toggleapproveModal();
+                window.location.reload();
+                
+            }
+            else{
+                let error = new Error('Error: ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            }
+        }, err => {
+            let error = new Error(err)
+            throw error
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    rejectDoctor(event)
+    {
+        event.preventDefault();
+        let doctors = this.state.doctors;
+        const userToken = localStorage.getItem('userToken');
+        const content = {
+            doctorId: doctors[this.state.doctorIndex]._id,
+            reason: this.reason.value,
+        }
+        fetch('/admin/rejectDoctor',{
+            method: 'PUT',
+            headers:{
+                'Authorization': 'Bearer ' + userToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(content)
+        })
+        .then((response) => {
+            if(response.ok)
+            {
+                this.togglerejectModal();
+                window.location.reload();
+            }
+            else{
+                let error = new Error('Error: ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            }
+        }, err => {
+            let error = new Error(err)
+            throw error
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    renderApproveModal()
+    {
+        return(
+            <Modal isOpen={this.state.approveModal} toggle={() => this.toggleapproveModal()}>
+                <ModalHeader toggle={() => this.toggleapproveModal()} className="forum-modal-header">
+                    Approve Doctor
+                </ModalHeader>
+                <ModalBody className="forum-modal-body">
+                    <Form onSubmit={this.approveDoctor} id="approveDoctorForm">
+                        <FormGroup>
+                            <Label htmlFor="specialization">Doctor's specialization</Label>
+                            <Input type="text" id="specialization" name="specialization" autoComplete="off" required
+                                innerRef={(input) => this.specialization = input} />
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter className="forum-modal-footer">
+                    <Button color="primary" type="submit" form="approveDoctorForm">Submit</Button>
+                    <Button color="danger" className="ml-2" onClick={() => this.toggleapproveModal()}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        )
+    }
+
+    renderRejectModal()
+    {
+        return(
+            <Modal isOpen={this.state.rejectModal} toggle={() => this.togglerejectModal()}>
+                <ModalHeader toggle={() => this.togglerejectModal()} className="forum-modal-header">
+                    Reject Doctor
+                </ModalHeader>
+                <ModalBody className="forum-modal-body">
+                    <Form onSubmit={this.rejectDoctor} id="rejectDoctorForm">
+                        <FormGroup>
+                            <Label htmlFor="reason">Reason for rejecting</Label>
+                            <Input type="textarea" id="reason" name="reason" className="forum-modal-textarea" rows="3" autoComplete="off" required 
+                                innerRef={(input) => this.reason = input} />
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter className="forum-modal-footer">
+                    <Button color="primary" type="submit" form="rejectDoctorForm">Submit</Button>
+                    <Button color="danger" className="ml-2" onClick={() => this.togglerejectModal()}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        )
     }
 
     render(){
@@ -131,6 +280,8 @@ class Approval extends Component {
                     <Header />
                     <div className="container forum-container mt-4 mb-4 p-4">
                         {this.renderDoctors()}
+                        {this.renderApproveModal()}
+                        {this.renderRejectModal()}
                     </div>
                 </>
             )   
