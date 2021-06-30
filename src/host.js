6eadde5aefa91ff69bc8c6ed0d1abca89a7e6b34
Aanandan-T-Ma <aanandan.tma@gmail.com>
2021-06-30 21:34:43 +0530

    host minor change

--- src/host.js
+++ src/host.js
@@ -1,3 +1,3 @@
 export const hostUrl = 'http://localhost:4000'
-export const videoCallUrl = 'http://localhost:5000'
-export const chatUrl = 'http://localhost:5001'
+export const videoCallUrl = 'http://localhost:5000/'
+export const chatUrl = 'http://localhost:5001/'
